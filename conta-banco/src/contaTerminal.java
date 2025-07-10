c64c3b0edfbb32c2deea5974b1ac6678476e325e
Thomas <thomas.vitor.cont@gmail.com>
2025-07-10 14:46:31 -0300

    Projeto finalizado

--- conta-banco/src/contaTerminal.java
+++ conta-banco/src/contaTerminal.java
@@ -1,8 +1,29 @@
+import java.util.Scanner;
+
 public class contaTerminal {
 
     public static void main(String[] args) throws Exception {
 
-        System.out.println("\n Hello, World!");
+        var scanner = new Scanner(System.in);
+
+        System.out.println("\nPor favor, digite o número da Agência: ");
+        var numero = scanner.nextInt();
+
+        System.out.println("\nPor favor, digite sua conta com o digito após o ''-''' : ");
+        var agencia = scanner.next();
+
+        System.out.println("\nPor favor, digite o seu primeiro nome: ");
+        var nome = scanner.next();
+
+        System.out.println("\nPor favor, digite o seu sobrenome: ");
+        var sobrenome = scanner.next();
+
+        var nomec = nome.concat(" " +sobrenome);
+
+        System.out.println("\nPor favor, digite o saldo da sua conta: ");
+        var saldo  = scanner.nextFloat();
+
+        System.out.printf("Olá %s ,obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.",nomec,agencia,numero,saldo);
 
     }
     
